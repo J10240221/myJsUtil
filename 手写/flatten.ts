191cc726ff218f45f5814366cdc6646d6eb40500
华志林 <linmu.hzl@raycloud.com>
2021-09-27 15:50:11 +0800

    完成 flat 拍平 函数

---
+++ 手写/flatten.ts
@@ -0,0 +1,22 @@
+/**
+ * 扁平化
+ * @param arr
+ * @param depth
+ */
+const flatten = (arr: any[], depth = 1) => {
+  let ret: any[] = [];
+
+  arr.forEach((item) => {
+    if (Array.isArray(item) && depth > 0) {
+      ret.push(...flatten(item, depth - 1));
+    } else {
+      ret.push(item);
+    }
+  });
+
+  return ret;
+};
+
+const aa = [[1, 2], [3, 4, [5]], [6]];
+
+console.log(flatten(aa));
