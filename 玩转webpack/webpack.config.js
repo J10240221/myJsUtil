0d73f7aa9ae899bc32f2bb8d0643de85c0a83bdc
huazhilin <Zhilin.Hua@zeekrlife.com>
2022-09-29 13:56:01 +0800

    feat: 完成 css 的压缩，提取，总结文档更新

--- 玩转webpack/webpack.config.js
+++ 玩转webpack/webpack.config.js
@@ -1,9 +1,20 @@
+'use strict';
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
 
   mode: 'development',
   devtool: 'cheap-source-map',
   module: {
-    rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] }],
+    rules: [{ test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }],
   },
+  plugins: [
+    // 压缩css 的插件
+    new CssMinimizerWebpackPlugin({ test: /\.css$/g }),
+    // 提取css到单文件的插件
+    new MiniCssExtractPlugin(),
+  ],
 };
